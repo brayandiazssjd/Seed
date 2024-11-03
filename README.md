b3280b3cd55cab0e05f7f5f3f1bf560c401ac60e
Brayan Diaz <brayandiazssjd@gmail.com>
2024-11-02 22:39:07 -0500

    First commit for the new proyect.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# nvim-config
-Personal Neovim configuration
+# Windows Snaptshot
+This repository is a colletion of settings I hve made in my Windows machine. Includes scripts for:
+- Install my programs and some fonts.
+- Export app settings like powerhsell profile, sublime text or neovim
+- Import your setting to this directory
---
+++ SublimeText.ps1
@@ -0,0 +1,5 @@
+# Sublime Text
+New-Item -Path .\SublimeText\ -ItermType Directory
+Copy-Item 'C:\Users\braya\AppData\Roaming\Sublime Text\Packages\User\Preferences.sublime-settings' .\SublimeText\
+Copy-Item 'C:\Users\braya\AppData\Roaming\Sublime Text\Packages\User\Package Control.sublime-settings' .\SublimeText\
+Copy-Item 'C:\Users\braya\AppData\Roaming\Sublime Text\Packages\User\Default (Windows).sublime-keymap' .\SublimeText\
